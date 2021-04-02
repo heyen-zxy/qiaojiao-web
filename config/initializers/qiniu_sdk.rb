c6fdbd6e001d8e5c194da517b62b71564428226f
heyen.zhang <heyen.zhang@easyke.com>
2021-04-02 16:10:22 +0800

    fix

--- config/initializers/qiniu_sdk.rb
+++ config/initializers/qiniu_sdk.rb
@@ -2,4 +2,4 @@ require 'qiniu'
 # 构建鉴权对象
 Qiniu.establish_connection! access_key: 'M5zN_IUlbAWlcHNRr1q7Cni8R9vuqTeY16Y0_fgW',
                             secret_key: 'IBjaVCD2zRPlPNtNhDrFI-TpUwNUvFpn8ZQCJCZV'
-Rails.application.config.qiniu_domain = 'http://file.jxjiajiang.com'
+Rails.application.config.qiniu_domain = 'https://file.jxjiajiang.com'
